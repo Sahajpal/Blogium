822585eb80088b1bf840c0efa106ef043ff545d8
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-05-30 15:52:10 +0530

    implemented posts section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
-import { Post } from '../typings';
+import { Post } from '../typings'
 
 interface Props {
-  posts: [Post];
+  posts: [Post]
 }
 
 export default function Home({ posts }: Props) {
@@ -35,6 +36,34 @@ export default function Home({ posts }: Props) {
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         ></img>
+      </div>
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/posts/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              ></img>
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                ></img>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
